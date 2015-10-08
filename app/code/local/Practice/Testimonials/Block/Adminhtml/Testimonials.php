c2427b02c540f5c0ff1beb77f999bf7c978643ae
Slavusja <slavunkacurly@rambler.ru>
2015-10-08 14:33:10 +0300

    delete public function _toHtml()

--- app/code/local/Practice/Testimonials/Block/Adminhtml/Testimonials.php
+++ app/code/local/Practice/Testimonials/Block/Adminhtml/Testimonials.php
@@ -1,8 +1,5 @@
 <?php
 class Practice_Testimonials_Block_Adminhtml_Testimonials extends Mage_Adminhtml_Block_Widget_Grid_Container {
-    public function _toHtml() {
-        return '<h1>News Module: Admin section</h1>';
-    }
     public function __construct() {
         $helper = Mage::helper('practicetestimonials');
         $this->_blockGroup = 'practicetestimonials';
@@ -10,6 +7,5 @@ class Practice_Testimonials_Block_Adminhtml_Testimonials extends Mage_Adminhtml_
         $this->_headerText = $helper->__('Testimonials Management');
         parent::__construct();
         $this->_addButtonLabel = $helper->__('Add Testimonials');
-
     }
 }
