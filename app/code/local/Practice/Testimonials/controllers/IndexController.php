a4c3699ebe53ebc3837e5c138394da7b20af2d64
Slavusja <slavunkacurly@rambler.ru>
2015-10-15 15:24:52 +0300

    add top link with helper, fix bug with input, textarea width of testimonials form

--- app/code/local/Practice/Testimonials/controllers/IndexController.php
+++ app/code/local/Practice/Testimonials/controllers/IndexController.php
@@ -26,14 +26,4 @@ class Practice_Testimonials_IndexController extends Mage_Core_Controller_Front_A
     public function getPagerHtml() {
         return $this->getChildHtml('pager');
     }
-    public function testAction() {
-        $params = $this->getRequest()->getParams();
-        $testimonials = Mage::getModel('practicetestimonials/testimonials');
-        echo("Loading the testimonials with an ID of ".$params['id']);
-        $testimonials->load($params['id']);
-        $data = $testimonials->getData();
-        echo "<pre>";
-        var_dump($data);
-        echo "</pre>";
-    }
 }
